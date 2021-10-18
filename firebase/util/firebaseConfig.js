737136d822339ef54a4341ac0ab7f77319661851
thechosenginger <sewell.matthewm@gmail.com>
2021-10-18 15:06:34 -0500

    Removed server file as it is unneeded, then added the hidden functionality of the api key

--- firebase/util/firebaseConfig.js
+++ firebase/util/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyChPdzyienxG7VRxEmJs-QlGXnce7LC7o8',
-  authDomain: 'red-paperclip-73a89.firebaseapp.com',
-  projectId: 'red-paperclip-73a89',
-  storageBucket: 'red-paperclip-73a89.appspot.com',
-  messagingSenderId: '47631862600',
-  appId: '1:47631862600:web:21468fb06c82a0628a017d',
-  measurementId: 'G-J77CK0PT1W',
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 export default firebaseConfig;

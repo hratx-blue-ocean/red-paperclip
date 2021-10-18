8b78a9d97da8b8697723afbaeba2bc0708475ae6
Pingping Xiao <ppx2021@gmail.com>
2021-10-18 17:00:37 -0500

    added initial containers for chatroom and text box

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import List from '@mui/material/List';
@@ -6,6 +7,7 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
+import Typography from '@mui/material/Typography';
 
 import { styled } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
@@ -60,19 +62,82 @@ const Chat = () => {
       <div className={classes.root}>
         <Grid container spacing={2}>
           <Grid item xs={4}>
-            <Stack direction="row" spacing={3}>
-              <Avatar alt="user" src="client/src/Components/Chat/img/Pingping.jpeg" />
-              <Avatar alt="item" src="client/src/Components/Chat/img/item.png" />
-            </Stack>
+            <div>Chat List</div>
+            <Grid container spacing={3}>
 
-            <Stack direction="row" spacing={3}>
-              <Avatar alt="user" src="client/src/Components/Chat/img/Pingping.jpeg" />
-              <Avatar alt="item" src="client/src/Components/Chat/img/item.png" />
-            </Stack>
+              <Grid item xs={6}>
+                <Avatar alt="user" src="client/src/Components/Chat/img/Pingping.jpeg" sx={{ width: 60, height: 60 }} />
+              </Grid>
+
+              <Grid item xs={6}>
+                <Avatar alt="item" src="client/src/Components/Chat/img/item.png" sx={{ width: 60, height: 60 }} />
+              </Grid>
+
+              <Grid item xs={6}>
+                <Avatar alt="user" src="client/src/Components/Chat/img/Pingping.jpeg" sx={{ width: 60, height: 60 }} />
+              </Grid>
+
+              <Grid item xs={6}>
+                <Avatar alt="item" src="client/src/Components/Chat/img/item.png" sx={{ width: 60, height: 60 }} />
+              </Grid>
+
+              <Grid item xs={12}>
+                <Button variant="contained">Archive Chat</Button>
+              </Grid>
+
+            </Grid>
           </Grid>
 
           <Grid item xs={8}>
             <div>Chat Room</div>
+            <Grid container spacing={3}>
+
+              <Grid item xs={3}>
+                <Avatar alt="user" src="client/src/Components/Chat/img/Jason.jpeg" sx={{ width: 60, height: 60 }} />
+              </Grid>
+
+              <Grid item xs={3}>
+                <Avatar alt="item" src="client/src/Components/Chat/img/item.png" sx={{ width: 60, height: 60 }} />
+              </Grid>
+
+              <Grid item xs={3}>
+                <Button variant="contained">Close Deal</Button>
+              </Grid>
+
+            </Grid>
+
+            <Grid container spacing={3}>
+
+              <Grid item xs={12}>
+
+                <Paper sx={{ maxWidth: 800, my: 4, mx: 'auto', p: 3 }}>
+                  <Grid item>
+
+                    <Grid container wrap="nowrap" spacing={2}>
+                      <Grid item>
+                        <Avatar>U</Avatar>
+                      </Grid>
+                      <Grid item xs>
+                        <Typography>Are you interested in trading your High End Knife Collection for my Set of 3 Axes?</Typography>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+
+                  <Grid container wrap="nowrap" spacing={2}>
+                    <Grid item>
+                      <Avatar>U</Avatar>
+                    </Grid>
+                    <Grid item xs>
+                      <Typography>I really like your item!</Typography>
+                    </Grid>
+                  </Grid>
+
+                </Paper>
+
+              </Grid>
+
+
+            </Grid>
             <form name="message" action="">
               <input name="usermsg" type="text" id="usermsg" />
               <input name="submitmsg" type="submit" id="submitmsg" value="Send" />

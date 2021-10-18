ebbb2f7c96118c7b96ac66b86239585f361a0eef
travis503 <travis503@gmail.com>
2021-10-18 12:00:01 -0500

    Added Jest

---
+++ jest.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-undef */
+it('Testing to see if Jest works', () => {
+  // eslint-disable-next-line prettier/prettier
+  expect(1).toBe(1);
+  // eslint-disable-next-line prettier/prettier
+  // eslint-disable-next-line prettier/prettier
+});

5c61faaa4b19ae0e3a08ab29e6ae45eaddf1dad4
Pingping Xiao <ppx2021@gmail.com>
2021-10-19 11:54:22 -0500

    improved layout with list, nav, and paper within Grid container

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -1,153 +1,114 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import List from '@mui/material/List';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
 import Avatar from "@mui/material/Avatar";
-import Stack from "@mui/material/Stack";
-import Typography from '@mui/material/Typography';
 
-import { styled } from '@mui/material/styles';
-import { makeStyles } from '@mui/styles';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#2C2C2C',
-    },
-    secondary: {
-      main: '#f0CC71',
-    },
-  },
-});
-
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
-
-const useStyles = makeStyles(() => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    textAlign: 'flex-start',
-    color: theme.palette.text.secondary,
-  },
-  imageList: {
-    flexWrap: 'nowrap',
-    transform: 'translateZ(0)',
-  },
-  bold: {
-    fontWeight: 600,
-  },
-  hover: {
-    color: '#2C2C2C',
-    '&:hover': {
-      color: '#f0CC71',
-    },
-  },
-}));
-
-const Chat = () => {
-  const classes = useStyles();
-
+export default function BasicList() {
   return (
-    <ThemeProvider theme={theme}>
-      <div className={classes.root}>
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <div>Chat List</div>
-            <Grid container spacing={3}>
+    // <Box sx={{ width: "100%", maxWidth: 300, bgcolor: "background.paper" }}>
+    <Container maxWidth="xl">
+      <Grid container spacing={3}>
+        <Grid item xs={4}>
+          <nav aria-label="secondary mailbox folders">
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton component="a" href="/{username}">
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>JW</Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Jesson W"
+                      secondary="Jan 7, 2014"
+                    />
+                  </ListItem>
+                </ListItemButton>
+              </ListItem>
+            </List>
+            <Divider />
+          </nav>
+          <nav aria-label="main mailbox folders">
+            <List>
+              <ListItemButton component="a" href="/{username}">
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>MS</Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary="Matt S" secondary="July 20, 2014" />
+                </ListItem>
+              </ListItemButton>
+              <ListItemButton component="a" href="/{username}">
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>BH</Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary="Blake H" secondary="July 20, 2014" />
+                </ListItem>
+              </ListItemButton>
+              <ListItemButton component="a" href="/{username}">
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>BC</Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary="Bekah C" secondary="July 20, 2014" />
+                </ListItem>
+              </ListItemButton>
+              <ListItemButton component="a" href="/{username}">
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>CE</Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary="Cory E" secondary="July 20, 2014" />
+                </ListItem>
+              </ListItemButton>
+              <ListItemButton component="a" href="/{username}">
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>TM</Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Travis M"
+                    secondary="July 20, 2014"
+                  />
+                </ListItem>
+              </ListItemButton>
 
-              <Grid item xs={6}>
-                <Avatar alt="user" src="client/src/Components/Chat/img/Pingping.jpeg" sx={{ width: 60, height: 60 }} />
-              </Grid>
+              <ListItemButton component="a" href="/{username}">
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>WF</Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary="Will F" secondary="July 20, 2014" />
+                </ListItem>
+              </ListItemButton>
 
-              <Grid item xs={6}>
-                <Avatar alt="item" src="client/src/Components/Chat/img/item.png" sx={{ width: 60, height: 60 }} />
-              </Grid>
-
-              <Grid item xs={6}>
-                <Avatar alt="user" src="client/src/Components/Chat/img/Pingping.jpeg" sx={{ width: 60, height: 60 }} />
-              </Grid>
-
-              <Grid item xs={6}>
-                <Avatar alt="item" src="client/src/Components/Chat/img/item.png" sx={{ width: 60, height: 60 }} />
-              </Grid>
-
-              <Grid item xs={12}>
-                <Button variant="contained">Archive Chat</Button>
-              </Grid>
-
-            </Grid>
-          </Grid>
-
-          <Grid item xs={8}>
-            <div>Chat Room</div>
-            <Grid container spacing={3}>
-
-              <Grid item xs={3}>
-                <Avatar alt="user" src="client/src/Components/Chat/img/Jason.jpeg" sx={{ width: 60, height: 60 }} />
-              </Grid>
-
-              <Grid item xs={3}>
-                <Avatar alt="item" src="client/src/Components/Chat/img/item.png" sx={{ width: 60, height: 60 }} />
-              </Grid>
-
-              <Grid item xs={3}>
-                <Button variant="contained">Close Deal</Button>
-              </Grid>
-
-            </Grid>
-
-            <Grid container spacing={3}>
-
-              <Grid item xs={12}>
-
-                <Paper sx={{ maxWidth: 800, my: 4, mx: 'auto', p: 3 }}>
-                  <Grid item>
-
-                    <Grid container wrap="nowrap" spacing={2}>
-                      <Grid item>
-                        <Avatar>U</Avatar>
-                      </Grid>
-                      <Grid item xs>
-                        <Typography>Are you interested in trading your High End Knife Collection for my Set of 3 Axes?</Typography>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-
-                  <Grid container wrap="nowrap" spacing={2}>
-                    <Grid item>
-                      <Avatar>U</Avatar>
-                    </Grid>
-                    <Grid item xs>
-                      <Typography>I really like your item!</Typography>
-                    </Grid>
-                  </Grid>
-
-                </Paper>
-
-              </Grid>
-
-
-            </Grid>
-            <form name="message" action="">
-              <input name="usermsg" type="text" id="usermsg" />
-              <input name="submitmsg" type="submit" id="submitmsg" value="Send" />
-            </form>
-          </Grid>
+              <ListItemButton component="a" href="/{username}">
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>PX</Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Pingping X"
+                    secondary="Jan 9, 2014"
+                  />
+                </ListItem>
+              </ListItemButton>
+            </List>
+          </nav>
         </Grid>
-      </div>
-    </ThemeProvider>
-
+        <Grid item xs={8}>
+          <Paper elevation={3} style={{ height: 450 }}></Paper>
+          <Paper elevation={3} style={{ height: 120 }}></Paper>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
-
-export default Chat;

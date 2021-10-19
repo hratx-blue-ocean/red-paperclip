9e6e2f0496999c13780c40be9ed252c0429d4ecf
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-10-19 15:53:01 -0500

    Style components

--- client/src/Components/Admin/ReportedMessage.js
+++ client/src/Components/Admin/ReportedMessage.js
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import StarIcon from '@mui/icons-material/Star';
 import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';

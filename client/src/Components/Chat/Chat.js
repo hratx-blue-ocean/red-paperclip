3dd2f3d0e5d1baea10dd2fa44693134471815bba
Pingping Xiao <ppx2021@gmail.com>
2021-10-19 14:28:10 -0500

    Added Boxs to chat history

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -12,120 +12,171 @@ import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
 import Avatar from "@mui/material/Avatar";
 
+
 export default function BasicList() {
   return (
-    <Container maxWidth="xl">
-      <Grid container spacing={3}>
-        <Grid item xs={4}>
-          <Paper elevation={3}>
-            <nav aria-label="secondary mailbox folders">
-              <List>
-                <ListItem disablePadding>
+    <Box m={2} pt={2}>
+      <Container maxWidth="xl">
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <Paper elevation={3}>
+              <nav aria-label="secondary mailbox folders">
+                <List>
+                  <ListItem disablePadding>
+                    <ListItemButton component="a" href="/{username}">
+                      <ListItem>
+                        <ListItemAvatar>
+                          <Avatar>JW</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary="Jesson W"
+                          secondary="Jan 7, 2014"
+                        />
+                      </ListItem>
+                    </ListItemButton>
+                  </ListItem>
+                </List>
+                <Divider />
+              </nav>
+              <nav aria-label="main mailbox folders">
+                <List>
                   <ListItemButton component="a" href="/{username}">
                     <ListItem>
                       <ListItemAvatar>
-                        <Avatar>JW</Avatar>
+                        <Avatar>MS</Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary="Matt S" secondary="July 20, 2014" />
+                    </ListItem>
+                  </ListItemButton>
+                  <ListItemButton component="a" href="/{username}">
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar>BH</Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary="Blake H" secondary="July 20, 2014" />
+                    </ListItem>
+                  </ListItemButton>
+                  <ListItemButton component="a" href="/{username}">
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar>BC</Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary="Bekah C" secondary="July 20, 2014" />
+                    </ListItem>
+                  </ListItemButton>
+                  <ListItemButton component="a" href="/{username}">
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar>CE</Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary="Cory E" secondary="July 20, 2014" />
+                    </ListItem>
+                  </ListItemButton>
+                  <ListItemButton component="a" href="/{username}">
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar>TM</Avatar>
                       </ListItemAvatar>
                       <ListItemText
-                        primary="Jesson W"
-                        secondary="Jan 7, 2014"
+                        primary="Travis M"
+                        secondary="July 20, 2014"
                       />
                     </ListItem>
                   </ListItemButton>
-                </ListItem>
-              </List>
-              <Divider />
-            </nav>
-            <nav aria-label="main mailbox folders">
-              <List>
-                <ListItemButton component="a" href="/{username}">
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar>MS</Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary="Matt S" secondary="July 20, 2014" />
-                  </ListItem>
-                </ListItemButton>
-                <ListItemButton component="a" href="/{username}">
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar>BH</Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary="Blake H" secondary="July 20, 2014" />
-                  </ListItem>
-                </ListItemButton>
-                <ListItemButton component="a" href="/{username}">
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar>BC</Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary="Bekah C" secondary="July 20, 2014" />
-                  </ListItem>
-                </ListItemButton>
-                <ListItemButton component="a" href="/{username}">
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar>CE</Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary="Cory E" secondary="July 20, 2014" />
-                  </ListItem>
-                </ListItemButton>
-                <ListItemButton component="a" href="/{username}">
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar>TM</Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary="Travis M"
-                      secondary="July 20, 2014"
-                    />
-                  </ListItem>
-                </ListItemButton>
 
-                <ListItemButton component="a" href="/{username}">
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar>WF</Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary="Will F" secondary="July 20, 2014" />
-                  </ListItem>
-                </ListItemButton>
+                  <ListItemButton component="a" href="/{username}">
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar>WF</Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary="Will F" secondary="July 20, 2014" />
+                    </ListItem>
+                  </ListItemButton>
 
-                <ListItemButton component="a" href="/{username}">
+                  <ListItemButton component="a" href="/{username}">
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar>PX</Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary="Pingping X"
+                        secondary="Jan 9, 2014"
+                      />
+                    </ListItem>
+                  </ListItemButton>
+                </List>
+              </nav>
+            </Paper>
+          </Grid>
+          <Grid item xs={8}>
+            <Paper elevation={6} style={{ height: 550 }}>
+              <nav aria-label="secondary mailbox folders">
+                <List>
+                  <ListItem>
+                    <ListItemButton component="a" href="/{username}">
+                      <ListItem>
+                        <ListItemAvatar>
+                          <Avatar>JW</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary="Jesson W"
+                          secondary="Trusted User"
+                        />
+                      </ListItem>
+                    </ListItemButton>
+                  </ListItem>
+                </List>
+                <Divider />
+              </nav>
+              <div style={{ width: '100%' }}>
+                <Box
+                  component="span"
+                  sx={{
+                    display: 'block',
+                    p: 1,
+                    m: 1,
+                    bgcolor: 'background.paper',
+                  }}
+                >
                   <ListItem>
                     <ListItemAvatar>
-                      <Avatar>PX</Avatar>
+                      <Avatar>JW</Avatar>
                     </ListItemAvatar>
-                    <ListItemText
-                      primary="Pingping X"
-                      secondary="Jan 9, 2014"
-                    />
+                    <ListItemText primary="Hi, I am interested to make a trade with you!" />
                   </ListItem>
-                </ListItemButton>
-              </List>
-            </nav>
-          </Paper>
+                </Box>
+                <Box
+                  component="span"
+                  sx={{
+                    display: 'block',
+                    p: 1,
+                    m: 1,
+                    bgcolor: 'background.paper',
+                  }}
+                >
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>JW</Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="How you like that?" />
+                  </ListItem>
+                </Box>
+              </div>
+            </Paper>
+            <Paper elevation={6} style={{ height: 170 }}>
+              <Box m={2} pt={2}>
+                <TextField
+                  fullWidth
+                  id="fullWidth"
+                  label="Private Chat Room With Jesson"
+                  multiline
+                  rows={4}
+                />
+              </Box>
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid item xs={8}>
-          <Paper elevation={6} style={{ height: 600 }}></Paper>
-          <Paper elevation={6} style={{ height: 150 }}>
-            <Box
-              sx={{
-                width: 800,
-                maxWidth: "100%"
-              }}
-            >
-              <TextField
-                fullWidth
-                id="fullWidth"
-                label="Private Chat Room With Jesson"
-                multiline
-                rows={5}
-              />
-            </Box>
-          </Paper>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </Box>
   );
 }

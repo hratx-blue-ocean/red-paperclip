ff57adb4441fc79c011f126a60157698c57d00b2
William Fink <wwfink30@gmail.com>
2021-10-19 13:09:41 -0500

    temp

--- firebase/functions/APIs/Items/reportItem.js
+++ firebase/functions/APIs/Items/reportItem.js
@@ -1,7 +1,7 @@
 const { db } = require('../../../util/admin');
 
 const reportItem = (req, res) => {
-  db.doc(`items/${req.query.uid}`)
+  db.doc(`items/${req.body.uid}`)
     .update('reports', db.FieldValue.increment(1))
     .then(() => res.end())
     .catch((err) => res.status(500).json({ error: err.code }));
--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,5 +1,8 @@
-const app = require('express')();
+const express = require('express');
 const functions = require('firebase-functions');
+
+const app = express();
+app.use(express.json());
 // const auth = require('../util/auth');
 // const {} = require('./APIs/users');
 const { item, user, message, admin } = require('./APIs');
@@ -13,7 +16,7 @@ app.get('/getAllItems', item.getAllItems);
 // app.get('/getItem', getItem);
 app.get('/getItem', item.getItem);
 app.get('/editItem', item.editItem);
-app.get('/addItem', item.addNewItem);
+app.post('/addItem', item.addNewItem);
 app.get('/reportItem', item.reportItem);
 
 const api = functions.https.onRequest(app);

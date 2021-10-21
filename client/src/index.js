99be2d32fd255e54510bb01811f157a63ba33329
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-10-21 12:21:22 -0500

    Minor refactor of theme

--- client/src/index.js
+++ client/src/index.js
@@ -4,38 +4,38 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './Components/App';
 import { ItemsProvider } from './Components/ItemsContext';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#FFFFFF',
-    },
-    secondary: {
-      main: '#F0CC71',
-    },
-    cardButton: {
-      main: '#161513',
-    },
-    cardColor: {
-      main: '#494D53',
-    },
-    textWhite: {
-      main: '#FFFFFF',
-    },
-    textYellow: {
-      main: '#F0CC71',
-    },
-    sortButton: {
-      main: '#2C2C2C',
-    },
-    paperClip: {
-      main: '#A10000',
-    },
-    text: {
-      white: '#FFFFFF',
-      gold: '#F0CC71',
-    },
-  },
-});
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: '#FFFFFF',
+//     },
+//     secondary: {
+//       main: '#F0CC71',
+//     },
+//     cardButton: {
+//       main: '#161513',
+//     },
+//     cardColor: {
+//       main: '#494D53',
+//     },
+//     textWhite: {
+//       main: '#FFFFFF',
+//     },
+//     textYellow: {
+//       main: '#F0CC71',
+//     },
+//     sortButton: {
+//       main: '#2C2C2C',
+//     },
+//     paperClip: {
+//       main: '#A10000',
+//     },
+//     text: {
+//       white: '#FFFFFF',
+//       gold: '#F0CC71',
+//     },
+//   },
+// });
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>

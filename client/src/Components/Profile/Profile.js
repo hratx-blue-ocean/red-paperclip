3eb6962aef9cde2bd30145e3e16a885d970913dd
travis503 <travis503@gmail.com>
2021-10-21 15:37:38 -0500

    Adds reactive scrollable watched items tab + data

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -14,7 +14,16 @@ import AddItem from './AddItem';
 const Profile = () => {
   const { currentUser } = useContext(ItemsContext);
   const user = currentUser[0];
-  console.log('Current user: ', currentUser);
+  // console.log('Current user: ', currentUser);
+
+  const testActiveItem = {
+    itemName: 'Rustic Axe Set 1',
+    itemDesc:
+      'These beautiful axes were custom made in the heart of Minnesota. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing. I need to have more characters so I am typing.',
+    itemOwner: 'Jeffrey Dahmer',
+    itemPicture:
+      'https://images.squarespace-cdn.com/content/v1/5acd0a3c8ab722892928be5a/1565878992439-2ER3KM60OHYNPNF2LPP3/8B059829-6D7F-424F-B9A7-4C44C112CFF9.jpg?format=2500w',
+  };
 
   return (
     <>
@@ -54,18 +63,17 @@ const Profile = () => {
           <Card
             sx={{
               width: 450,
-              height: 550,
+              height: 600,
               backgroundColor: '#494D53',
               marginTop: 5,
+              marginBottom: 10,
             }}
           >
             <Typography variant="h4" textAlign="center" sx={{ marginTop: 1 }}>
               Active Item
             </Typography>
-            <ProfileActiveItem />
+            <ProfileActiveItem activeItem={testActiveItem} height="50%" />
           </Card>
-
-
 
           <Card
             sx={{
@@ -91,9 +99,6 @@ const Profile = () => {
               </Grid>
             </Grid>
           </Card>
-
-
-
         </Grid>
         <Grid item xs={7} sx={{ marginTop: 1 }}>
           <ProfileTabs />

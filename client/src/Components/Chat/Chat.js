b7530a91af9fcd2c4b4b95c9e06b901524e8a25a
Pingping Xiao <ppx2021@gmail.com>
2021-10-21 10:29:23 -0500

    clean up code

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -1,7 +1,4 @@
 import * as React from 'react';
-
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 
 import ChatList from './ChatList.js';
--- client/src/Components/Chat/ChatConversation.js
+++ client/src/Components/Chat/ChatConversation.js
@@ -1,18 +1,16 @@
 import * as React from 'react';
-import List from '@mui/material/List';
-import TextField from '@mui/material/TextField';
+
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemButton from '@mui/material/ListItemButton';
+
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import MdPhone from '@mui/icons-material/Phone';
-import Chip from '@mui/material/Chip';
+
 import DeleteIcon from '@mui/icons-material/Delete';
 import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
 import { makeStyles } from '@mui/styles';

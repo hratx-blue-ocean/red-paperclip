5c5c179638c7047cd24fd94c533dac607b9e4dfa
Rebekah Cruz <rebekah.cruz702@gmail.com>
2021-10-22 16:56:32 -0500

    create info window on map

--- client/src/Components/Profile/Maps.js
+++ client/src/Components/Profile/Maps.js
@@ -1,48 +1,75 @@
-import React from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React, { useContext } from 'react';
+import {
+  GoogleMap,
+  LoadScript,
+  InfoWindow,
+  Marker,
+} from '@react-google-maps/api';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { ItemsContext } from '../ItemsContext';
 
-const containerStyle = {
-  width: '400px',
-  height: '400px',
+const mapContainerStyle = {
+  width: '450px',
+  height: '550px',
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523,
+const centers = [
+  {
+    lat: 30.266666,
+    lng: -97.73333,
+  },
+  {
+    lat: 30.633263,
+    lng: -97.677986,
+  },
+  {
+    lat: 30.6333,
+    lng: -97.678,
+  },
+];
+
+const position = { lat: 30.266666, lng: -97.73333 };
+
+const divStyle = {
+  background: `white`,
+  border: `1px solid #ccc`,
+  padding: 15,
 };
 
-function MyComponent() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: 'AIzaSyCC5wJDdHBdkKORms0TLd1qCGWB2cr3SGs',
-  });
+const onLoad = (infoWindow) => {
+  console.log('infoWindow: ', infoWindow);
+};
 
-  const [userMap, setUserMap] = React.useState(null);
+function Maps() {
+  const { isLoggedInState, menuOpenState, currentUserState } =
+    useContext(ItemsContext);
+  const [currentUser, setCurrentUser] = currentUserState;
+  const [isLoggedIn, setIsLoggedIn] = isLoggedInState;
 
-  const onLoad = React.useCallback((map) => {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setUserMap(map);
-  }, []);
-
-  const onUnmount = React.useCallback((map) => {
-    setUserMap(null);
-  }, []);
-
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      {/* Child components, such as markers, info windows, etc. */}
-      <></>
-    </GoogleMap>
-  ) : (
-    <></>
+  return (
+    <Grid container justifyContent="center">
+      <Grid item align="center">
+        <LoadScript googleMapsApiKey="AIzaSyCC5wJDdHBdkKORms0TLd1qCGWB2cr3SGs">
+          <GoogleMap
+            id="InfoWindow-example"
+            mapContainerStyle={mapContainerStyle}
+            zoom={10}
+            center={centers[0]}
+          >
+            <InfoWindow onLoad={onLoad} position={position}>
+              <div style={divStyle}>
+                <h1>InfoWindow</h1>
+              </div>
+            </InfoWindow>
+          </GoogleMap>
+        </LoadScript>
+      </Grid>
+      <Grid item align="center">
+        <Typography>Item history for items: </Typography>
+      </Grid>
+    </Grid>
   );
 }
 
-export default React.memo(MyComponent);
+export default React.memo(Maps);

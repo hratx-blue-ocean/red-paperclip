49006d9fa7969f82fdbbe3726893e45be124e417
Pingping Xiao <ppx2021@gmail.com>
2021-10-26 01:25:39 -0500

    added timeago, made small changes to chatList layout

--- client/src/Components/Chat/ChatList/ChatListEntry.js
+++ client/src/Components/Chat/ChatList/ChatListEntry.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import '../Chats.css';
 import Avatar from '@mui/material/Avatar';
+import timeago from 'epoch-timeago';
+import Divider from '@mui/material/Divider';
 import { ItemsContext } from '../../ItemsContext';
 
 export default function ChatListEntry({ entryObj }) {
@@ -21,29 +23,43 @@ export default function ChatListEntry({ entryObj }) {
   // once clicked
   // sets the global state of currentChatRoom
 
+  const TimeAgo = ({ time }) => (
+    <time dateTime={new Date(time).toISOString()}>{timeago(time)}</time>
+  );
+
+  const epochTimeStamp = Date.now() - 60000 * 22;
+  <TimeAgo time={epochTimeStamp} />;
+
   return (
     <>
       {trader1 === currentUser.firstName ? (
         <>
           <div className="chat">
-            <Avatar className="chat_image" src={trader2Photo} />
+            <div>
+              <Avatar className="chat_image" src={trader2Photo} />
+              <h3>{trader2}</h3>
+            </div>
             <div className="chat_details">
-              <h2>{trader2}</h2>
               <p>{lastMessage}</p>
             </div>
-            <p className="chat_timestamp">{timestamp}</p>
+            <p className="chat_timestamp">
+              {' '}
+              <TimeAgo time={epochTimeStamp} />
+            </p>
           </div>
+          <Divider />
         </>
       ) : (
         <>
           <div className="chat">
             <Avatar className="chat_image" src={trader1Photo} />
+            <h3>{trader1}</h3>
             <div className="chat_details">
-              <h2>{trader1}</h2>
               <p>{lastMessage}</p>
             </div>
             <p className="chat_timestamp">{timestamp}</p>
           </div>
+          <Divider />
         </>
       )}
     </>
